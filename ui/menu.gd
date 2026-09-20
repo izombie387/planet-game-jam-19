91e7fe97d2bf47415fa8a10f6573d188550c3216
Emiria-a <pipouille2003@gmail.com>
2026-09-20 23:59:05 +0200

    the menu appear/disappear on escape key pressed

--- ui/menu.gd
+++ ui/menu.gd
@@ -6,6 +6,11 @@ extends CenterContainer
 func _ready() -> void:
 	close_button.pressed.connect(hide)
 	volume_slider.value_changed.connect(_volume_changed.bind(0))
-	
+
+func _process(delta: float) -> void:
+	# the menu appear if escape key is pressed
+	if (Input.is_action_just_pressed("ui_cancel")):
+		visible = not visible
+
 func _volume_changed(val: float, bus_idx: int) -> void:
 	AudioServer.set_bus_volume_linear(bus_idx, val)
