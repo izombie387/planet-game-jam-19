df847382f607d7b5e5fbe92ad631a82fc70e7448
izombie387 <songsmadeforyou@gmail.com>
2026-09-20 20:36:15 -0700

    bag puzzle prototype working

--- blocks/bag_puzzle.gd
+++ blocks/bag_puzzle.gd
@@ -1,9 +1,9 @@
 extends Control
 
-#const HALF_CELL := Vector2(8,8)
-var _dragging_shape : Node2D
+var _dragging_shape : OreShape
 @export var ore_container: Control
 @export var map: TileMapLayer
+@export var _debug_cells := false
 var _cells: Dictionary[Vector2i, OreShape]
 
 func _ready() -> void:
@@ -20,24 +20,43 @@ func _on_area_input(_vp: Node, event: InputEvent, _idx: int, ore_shape: Node2D) 
 		_dragging_shape.modulate.a = 0.75
 		_dragging_shape.z_index += 10
 		
+		_debug_show_cells()
+		
 	elif event.is_action_released("select"):
 		if not _dragging_shape == ore_shape:
 			return
 		if _try_drop(get_global_mouse_position(), _dragging_shape):
 			pass
 		else:
-			_dragging_shape.reset_position()
-			_try_drop(_dragging_shape.global_position, _dragging_shape)
+			_dragging_shape.queue_free()
+			_dragging_shape = null
+			return
+			#_dragging_shape.reset_position()
+			#_try_drop(_dragging_shape.global_position, _dragging_shape)
 		_dragging_shape.modulate.a = 1.0
 		_dragging_shape.z_index -= 10
 		_dragging_shape = null
+		
+		_debug_show_cells()
+		
+	elif event.is_action_pressed("rotate_shape"):
+		if not _dragging_shape:
+			return
+		_dragging_shape.rotate_90()
+		
+func _debug_show_cells() -> void:
+	for cell in map.get_used_cells():
+		if cell in _cells:
+			map.set_cell(cell, 0, Vector2i(2,0))
+		else:
+			map.set_cell(cell, 0, Vector2i(0,1))
 		
 func _try_drop(drop_pos: Vector2, shape: OreShape) -> bool:
 	assert(shape)
 	if not shape:
 		return false
 	var origin = map.local_to_map(drop_pos)
-	var grid = map.get_used_cells()
+	
 	for offset: Vector2i in shape.cell_offsets:
 		var c = origin + offset
 		if map.get_cell_source_id(c) == -1:
@@ -62,3 +81,5 @@ func _pickup(shape: OreShape) -> void:
 func _process(_delta: float) -> void:
 	if _dragging_shape:
 		_dragging_shape.global_position = get_global_mouse_position()
+		
+				
--- blocks/ore_shape.gd
+++ blocks/ore_shape.gd
@@ -2,47 +2,72 @@
 class_name OreShape
 extends Node2D
 
-@export_tool_button("find cells") var fc = _set_cell_offsets
+## Root node is -8,-8 position
+## this is just so editing the poly line snaps correctly
+## shouldn't have any negative effect when instantitated
+
+@export_tool_button("find cells") var fc = _find_cells
 @export_tool_button("transfer polygon") var tp = _transfer_polygon
 
 @export var area: Area2D
 @export var cell_offsets: Array[Vector2i]
 @export var collision: CollisionPolygon2D
 @export var poly: Polygon2D
+@export var _debug_draw_cells := false
+@export var outline: Node2D
 
 var starting_position : Vector2
 
+var debug_rotated_polygon
+
 func _ready() -> void:
+	_find_cells()
 	starting_position = global_position
 	area.input_pickable = true
+	
+func rotate_90() -> void:
+	rotation += angle_difference(rotation, rotation + PI / 2.0)
+	_rotate_cell_offsets()
+	
+func _draw() -> void:
+	if not _debug_draw_cells:
+		return
+		
+	if cell_offsets:
+		for c in cell_offsets:
+			var rect = Rect2(
+					Vector2(c) * TileManager.CELL_SIZE - TileManager.CELL_SIZE / 2.0, 
+					TileManager.CELL_SIZE * 1.2)
+			draw_rect(rect, Color(0.0, 0.357, 1.0, 1.0))
+			
+	if debug_rotated_polygon:
+		draw_polyline(debug_rotated_polygon, Color.YELLOW, 1.0)
+	if poly.polygon:
+		draw_polyline(poly.polygon, Color.WHITE, 4.0)
 	
 func _transfer_polygon() -> void:
 	collision.polygon = poly.polygon
 	
-#func place_at_cell(cell: Vector2i, map: TileMapLayer) -> void:
-	#var new_position = map.local_to_map(cell)
-	#global_position = new_position + TileManager.HALF_CELL
-	#
-#func move_to_position(new_pos: Vector2) -> void:
-	#global_position = new_pos + TileManager.HALF_CELL
-	#
-#func get_origin() -> Vector2:
-	#return global_position - TileManager.HALF_CELL
-	
 func reset_position() -> void:
 	global_position = starting_position
 
-func _set_cell_offsets() -> void:
-	if not collision.polygon:
+func _rotate_cell_offsets() -> void:
+	cell_offsets.assign(cell_offsets.map(func(c): return Vector2i(-c.y, c.x)))
+
+func _find_cells() -> void:
+	if not poly.polygon:
+		assert(false)
 		return
-	var cell_size = Vector2(16,16)
-	var origin = -cell_size / 2.0
+
 	var cells: Array[Vector2i]
-	for x in range(-3,3,1):
-		for y in range(-3,3,1):
-			var p = Vector2(x,y) * cell_size + origin
-			var is_inside = Geometry2D.is_point_in_polygon(p, collision.polygon)
-			if is_inside:
+	for x in range(-3,3):
+		for y in range(-3,3):
+			var check_point = Vector2(x - 0.5, y - 0.5) * TileManager.CELL_SIZE
+			if Geometry2D.is_point_in_polygon(check_point, poly.polygon):
 				cells.append(Vector2i(x,y))
+				
 	cell_offsets = cells
-	print("Found %d cells" % cells.size())
+
+	if _debug_draw_cells:
+		queue_redraw()
+		
